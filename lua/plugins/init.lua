f1bb2a916063107f8687234981cbf79beabfb0ab
fomotrader <nerdcoiniac@gmail.com>
2023-05-13 22:31:54 +0100

    Add friendly-snippets

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -60,6 +60,7 @@ local plugins = {
 			-- snippets engine
 			{
 				"L3MON4D3/LuaSnip",
+				dependencies = { "rafamadriz/friendly-snippets" },
 				config = function()
 					require("luasnip.loaders.from_vscode").lazy_load()
 				end,
